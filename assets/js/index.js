f92438473f95e0e1555d744419f4d18c474c5109
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-09-09 23:53:34 -0500

    established connection to xml call and successfully parsed request

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
 console.log("Connected to ./assets/js/index.js");
+
+fetch("https://www.brighttalk.com/channel/19195/feed")
+    .then(response => {
+        console.log(`This is the response: ${response}`);
+        return response.text();
+
+    })
+    .then(str => {
+        console.log(`This is the string:  ${str}`);
+         return new DOMParser().parseFromString(str, "text/xml");
+    })
+    .then(data => {
+        console.log(data);
+    });
