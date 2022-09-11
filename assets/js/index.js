e4a5433182d80baa2bebc935e8b8a764c9a4d7fb
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-09-11 10:07:02 -0500

    added containers and access to xml document.element

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,27 @@
 console.log("Connected to ./assets/js/index.js");
 
+// create html container for brightTalk
+var documentBody = document.getElementsByTagName("body");
+console.log(documentBody[0]);
+var brightTalkContainerEl = document.createElement("div");
+
+// append container to body
+documentBody[0].insertBefore(brightTalkContainerEl, documentBody[0].children[0]);
+
+// insert video
+function createVideoEl(data) {
+    let videoContainer = document.querySelector("div");
+    console.log(data.documentElement);
+}
+
+
 fetch("https://www.brighttalk.com/channel/19195/feed")
     .then(response => {
-        console.log(`This is the response: ${response}`);
         return response.text();
-
     })
     .then(str => {
-        console.log(`This is the string:  ${str}`);
          return new DOMParser().parseFromString(str, "text/xml");
     })
     .then(data => {
-        console.log(data);
+        createVideoEl(data);
     });
