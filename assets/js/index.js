ee6405f0a40ec41dcf1f28f90026d2e810c45be7
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-09-25 11:12:25 -0500

    XML XPath comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
-console.log("Connected to ./assets/js/index.js");
+//console.log("Connected to ./assets/js/index.js");
 
 // create html container for brightTalk
 var documentBody = document.getElementsByTagName("body");
-console.log(documentBody[0]);
+//console.log(documentBody[0]);
 var brightTalkContainerEl = document.createElement("div");
 
 // append container to body
@@ -11,17 +11,29 @@ documentBody[0].insertBefore(brightTalkContainerEl, documentBody[0].children[0])
 // insert video
 function createVideoEl(data) {
     let videoContainer = document.querySelector("div");
+    console.log(data);
     console.log(data.documentElement);
+    console.log(data.documentElement.children);
+    console.log(data.documentElement.children[5]);
+    console.log(data.documentElement.children[5].href);
+    videoContainer.innerHTML = data.documentElement.children;
 }
 
 
 fetch("https://www.brighttalk.com/channel/19195/feed")
     .then(response => {
+        //console.log(response);
         return response.text();
     })
     .then(str => {
+        //console.log(str);
          return new DOMParser().parseFromString(str, "text/xml");
     })
     .then(data => {
         createVideoEl(data);
     });
+
+/* data.documentElement contains entries.These entries hold the title, summary, thumbnail
+   and video link. Look into using XML XPath to access the attributes of the XML elements.
+*/
+   
