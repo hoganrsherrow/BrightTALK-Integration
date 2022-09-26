e62ad4fd4a04458355ff81bbae6c7e7a3c9d8f29
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-09-25 21:05:02 -0500

    created new XMLhttp request object

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,30 @@
 //console.log("Connected to ./assets/js/index.js");
+const req = new XMLHttpRequest();
 
-// Use document.evaluate() ref https://developer.mozilla.org/en-US/docs/Web/XPath/Introduction_to_using_XPath_in_JavaScript
-//const xpathResult = document.evaluate();
+req.open('GET', 'https://www.brighttalk.com/channel/19195/feed', false);
+req.send(null);
 
+const xmlDoc = req.responseXML;
+console.log(xmlDoc);
 
-// create html container for brightTalk
-var documentBody = document.getElementsByTagName("body");
-//console.log(documentBody[0]);
-var brightTalkContainerEl = document.createElement("div");
+const nsResolver = xmlDoc.createNSResolver( xmlDoc.ownerDocument === null ? xmlDoc.documentElement : xmlDoc.ownerDocument.documentElement);
 
-// append container to body
-documentBody[0].insertBefore(brightTalkContainerEl, documentBody[0].children[0]);
+const titleIterator = xmlDoc.evaluate('//title', xmlDoc, nsResolver, XPathResult.ANY_TYPE, null);
+console.log(titleIterator);
+console.log(titleIterator.stringValue);
 
-// insert video
-function createVideoEl(data) {
-    const resolver = () => {
-        return 'http://www.w3.org/2005/Atom'
-    }
-    let videoContainer = document.querySelector("div");
-    console.log(data);
-    xmlDoc = data.documentElement
-    console.log(xmlDoc);
-    console.log(data.evaluate('//updated', data, resolver, XPathResult.ANY_TYPE, null));
-    //console.log(data.documentElement.children);
-    //console.log(data.documentElement.children[5]);
-    //console.log(data.documentElement.children[5].href);
-    videoContainer.innerHTML = data.documentElement.children;
-}
+// fetch("https://www.brighttalk.com/channel/19195/feed")
+//     .then(response => {
+//         //console.log(response);
+//         return response.text();
+//     })
+//     .then(str => {
+//         //console.log(str);
+//          return new DOMParser().parseFromString(str, "text/xml");
+//     })
+//     .then(data => {
+//         createVideoEl(data);
+//     });
 
 
-fetch("https://www.brighttalk.com/channel/19195/feed")
-    .then(response => {
-        //console.log(response);
-        return response.text();
-    })
-    .then(str => {
-        //console.log(str);
-         return new DOMParser().parseFromString(str, "text/xml");
-    })
-    .then(data => {
-        createVideoEl(data);
-    });
-
-/* data.documentElement contains entries.These entries hold the title, summary, thumbnail
-   and video link. Look into using XML XPath to access the attributes of the XML elements.
-*/
    
