cc04ecee44b8f06878be6a7d033b4bb0fc3cdf7b
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-09-26 23:11:22 -0500

    Succesfully grabbed all href values from all links

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,28 @@
+// const getHref = () => {
+//     const entries = 
+// }
+
+
+
 fetch("https://www.brighttalk.com/channel/19195/feed")
     .then(response => response.text())
     .then(str => new DOMParser().parseFromString(str, "text/xml"))
     .then(data => {
         console.log(data);
-        return data.evaluate("/*[name()='feed']/*[name()='entry']/*[name()='link']/@*[name()='href']", data, /*data.ownerDocument === null ? data.documentElement : data.ownerDocument.documentElement*/null, XPathResult.STRING_TYPE, null)
+        return data.evaluate("//*[name()='entry']//*[name()='link']//@*[name()='href']", data, /*data.ownerDocument === null ? data.documentElement : data.ownerDocument.documentElement*/null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null)
     })
-    .then(xmlDoc => console.log(xmlDoc.stringValue));
+    .then(xmlDoc => {
+        let thisNode = xmlDoc.iterateNext();
+        while(thisNode) {
+            console.log(thisNode.textContent);
+            thisNode = xmlDoc.iterateNext();
+        }
+    });
     
 
 /* data.documentElement contains entries.These entries hold the title, summary, thumbnail
    and video link. Look into using XML XPath to access the attributes of the XML elements.
+
+    Accessing HREFs: /@*[name()='href']
 */
    
