79cd63114f1202439d0d26474e4ae8109409a073
hoganrsherrow <hogan.r.sherrow@vanderbilt.edu>
2022-10-04 21:12:31 -0500

    btn successfully renders six additional webinars

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,6 @@
+// xml data is indexed at 1, not 0
+let i = 1;
+
 const createContainerRowEl = () => {
     let container = document.getElementById("brighttalk-container");
     let row = document.createElement("div");
@@ -28,9 +31,9 @@ const getHref = (results) => {
     for (let i = 0; i < 3; i++) {
         let thisNode = results.iterateNext();
         attributes.push(thisNode.textContent);
-        console.log(thisNode.textContent);
+        //console.log(thisNode.textContent);
     }
-    console.log(attributes);
+    //console.log(attributes);
     createWebinarEl(attributes);
 }
 
@@ -39,29 +42,46 @@ const createBtnEl = () => {
     let btn = document.createElement("div");
     btn.classList.add("fintalk-posts-load-more-wrap");
     btn.innerHTML = `
-                    <a href="javascript:alert('I was clicked');" class="fintalk-posts-load-more wp-block-button__link">
+                    <a href="javascript:getXmlData();" class="fintalk-posts-load-more wp-block-button__link" id="brighttalk-btn">
                         <span class="fintalk-posts-load-more-loader" style="display: none;">
                             <img src="https://tipalti.com/wp-content/themes/tipalti_accelerated/images/spin1.jpeg" alt="The BrightTALK Webinars">
                         </span>
                         Load More
                     </a>
                 `;
-    container.appendChild(btn);
+    container.append(btn);
+};
+
+const getXmlData = () => {
+    fetch("https://www.brighttalk.com/channel/19195/feed")
+        .then(response => response.text())
+        .then(str => new DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            //console.log(data);
+            xPathResults = data.evaluate(`//*[name()='entry'][position() >= ${i} and not(position() > ${i + 5})]//*[name()='link']//@*[name()='href']`, data, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+            i += 6;
+            return xPathResults;
+        })
+        .then(xPathResults => {
+            //console.log(xPathResults);
+            createContainerRowEl();
+            for (let i = 0; i < 6; i++) {
+                getHref(xPathResults);
+            }
+            let btn = document.getElementById("brighttalk-btn");
+            if(!btn) {
+                createBtnEl();
+            }
+        })
+        .catch(err => console.log(err));
 };
 
 
-fetch("https://www.brighttalk.com/channel/19195/feed")
-    .then(response => response.text())
-    .then(str => new DOMParser().parseFromString(str, "text/xml"))
-    .then(data => {
-        console.log(data);
-        return data.evaluate("//*[name()='entry'][position()<7]//*[name()='link']//@*[name()='href']", data, /*data.ownerDocument === null ? data.documentElement : data.ownerDocument.documentElement*/ null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null)
-    })
-    .then(xPathResults => {
-        console.log(xPathResults);
-        createContainerRowEl();
-        for (let i = 0; i < 6; i++) {
-            getHref(xPathResults);
-        };
-        createBtnEl();
-    });
+getXmlData();
+//createBtnEl();
+
+/* 
+Restructure fetch request to be function that executes on load and then on btn click. Can use i counter to keep track.
+Potentially look at using iterateNext() instead of for loop in getXmlData()
+so that you will not run into errors when trying to load more webinars. 
+*/
